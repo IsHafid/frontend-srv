69efb58ff09cd7aeb1f9aaba9d50cd0d946df4f9
IsmailHaf <53225171+IsmailHaf@users.noreply.github.com>
2022-12-13 15:45:52 +0100

    bill-detail

--- src/app/bill-details/bill-details.component.ts
+++ src/app/bill-details/bill-details.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {ActivatedRoute, Router} from "@angular/router";
 
 @Component({
   selector: 'app-bill-details',
   templateUrl: './bill-details.component.html',
   styleUrls: ['./bill-details.component.css']
 })
-export class BillDetailsComponent {
+export class BillDetailsComponent implements OnInit{
+  billDetails: any;
+  billId!: number;
+  constructor(private http:HttpClient, private router: Router, private route: ActivatedRoute) {
+    this.billId=route.snapshot.params['id'];
+  }
+  ngOnInit(): void {
+    this.http.get("http://localhost:8888/BILLING-SERVICE/fullBill/"+this.billId).subscribe({
+      next :(data)=>{
+        this.billDetails=data;
+      },
+      error : (err)=>{}
+    })
+  }
+  getBillDetails(b: any) {
+    this.router.navigateByUrl("/bill-details/"+b.id)
+  }
 
 }
